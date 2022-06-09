57072c57029ed39d93878eea98dedfb982ff6b09
Mario <daniellopezbosque@gmail.com>
2022-06-08 23:44:28 -0600

    listo2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = require("./app");
 require("./database");
 
 // starting the server
+//start
 app.listen(app.get("port"), () => {
   console.log(`server on port ${app.get("port")}`);
 });
