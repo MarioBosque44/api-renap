36a1a39c0d14ad53f20758d2869329f3953ac344
Mario <daniellopezbosque@gmail.com>
2022-06-08 23:51:31 -0600

    listo2

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.listen(PORT, function(){
 })
 
 app.get('/' , function(req, res){
-    res.send('Hola Dani')
+    res.send('[{"_id":"62a17a196ec034ad5e885b6c","firstName":"Daniel","lastName":"Aldamas","birthDat":"2022-05-06T00:00:00.000Z","birthFrom":"Quetzaltenango","DPI":"5013","genre":"Masculino","father":"Mario Bosque","mother":"Sofia Galicia","token":[],"createdAt":"2022-06-09T04:42:01.443Z","updatedAt":"2022-06-09T05:30:48.234Z"},{"_id":"62a185a0c562b1afee4fe0cf","firstName":"Jose","lastName":"Leiva","birthDat":"2022-02-06T00:00:00.000Z","birthFrom":"Guatemala","DPI":"4545","genre":"Masculino","father":"Jose Miguel","mother":"Darlen leiva","token":[],"createdAt":"2022-06-09T05:31:12.489Z","updatedAt":"2022-06-09T05:31:19.550Z"}]')
 })
 // Middlewares
 // const corsOptions = {origin: "http://localhost:4200"}
